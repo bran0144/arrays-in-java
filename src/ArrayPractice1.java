254dea7e445f44efb436111da0d81f9e2c85f1e4
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-04-23 14:35:49 -0500

    seventh challenge and cleaned up code

--- src/ArrayPractice1.java
+++ src/ArrayPractice1.java
@@ -2,37 +2,32 @@ public class ArrayPractice1 {
     public boolean firstLast6(int[] nums) {
         if(nums[0] == 6) {
             return true;
-        }else if(nums[nums.length -1] == 6) {
-            return true;
-        }else {
-            return false;
-        }
+        }else return nums[nums.length - 1] == 6;
     }
     public boolean sameFirstLast(int[] nums) {
-        if(nums.length >= 1 && nums[0] == nums[nums.length-1]) {
-            return true;
-        }else {
-            return false;
-        }
+        return nums.length >= 1 && nums[0] == nums[nums.length - 1];
     }
     public int[] makePi() {
         int[] piArray = new int[]{ 3,1,4 };
         return piArray;
     }
     public boolean commonEnd(int[] a, int[] b) {
-        if(a[0] == b[0] || a[a.length-1] == b[b.length-1]){
-            return true;
-        }else{
-            return false;
-        }
+        return a[0] == b[0] || a[a.length - 1] == b[b.length - 1];
 
     }
     public int sum3(int[] nums) {
         return nums[0] + nums[1] + nums[2];
     }
+
     public int[] rotateLeft3(int[] nums) {
         int[] intArray = new int[]{ nums[1], nums[2], nums[0]};
         return intArray;
     }
+
+    public int[] reverse3(int[] nums) {
+        int[] intArray = new int[]{ nums[2], nums[1], nums[0]};
+        return intArray;
+    }
+
 
 }
