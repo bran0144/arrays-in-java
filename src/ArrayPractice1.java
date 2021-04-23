16424672ea8242446ab1b1da103633c4c7b7a5d9
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-04-23 14:27:34 -0500

    fourth array

--- src/ArrayPractice1.java
+++ src/ArrayPractice1.java
@@ -19,6 +19,14 @@ public class ArrayPractice1 {
         int[] piArray = new int[]{ 3,1,4 };
         return piArray;
     }
+    public boolean commonEnd(int[] a, int[] b) {
+        if(a[0] == b[0] || a[a.length-1] == b[b.length-1]){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
 
 
 }
